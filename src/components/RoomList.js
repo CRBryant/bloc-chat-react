4aca8164a1b175677c012d79888fa9fafc77d1e1
Carly Rucker <carlyrucker@Carlys-MacBook-Pro.local>
2018-02-17 21:18:26 -0500

    checkpoint-3-add-rooms:completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -5,7 +5,10 @@ class RoomList extends Component {
     super(props);
     this.state = {
       rooms: [],
+      roomName: '',
     };
+    this.setRoomName = this.setRoomName.bind(this);
+    this.createRoom = this.createRoom.bind(this);
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
@@ -17,9 +20,26 @@ class RoomList extends Component {
     });
   }
 
+  createRoom(event) {
+    event.preventDefault();
+    this.roomsRef.push({
+      name: this.state.roomName
+    });
+    this.setState({ roomName: '' });
+  }
+
+  setRoomName(e) {
+    const newRoomName = e.target.value;
+    this.setState({roomName: newRoomName});
+  }
+
   render() {
     return (
       <div id='rooms'>
+        <form onSubmit={this.createRoom}>
+          <input type='text' onChange={this.setRoomName} />
+          <input type='submit' value='Create Room!'/>
+        </form>
         <ul className='roomsList'>
           {this.state.rooms.map( (room, index) =>
               <li key={index}>{room.name}</li>
